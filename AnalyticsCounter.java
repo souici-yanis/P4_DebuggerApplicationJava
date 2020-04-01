5615bbeec8f871b6fadb5e484c0ef9c0fbacec4d
souici-yanis <46894202+souici-yanis@users.noreply.github.com>
2020-04-01 14:17:49 +0200

    comment JavaDoc

--- AnalyticsCounter.java
+++ AnalyticsCounter.java
@@ -3,70 +3,81 @@ package com.hemebiotech.analytics;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class AnalyticsCounter implements ISymptomReader{
+public class AnalyticsCounter implements ISymptomReader {
 	String urlFile;
 
-	AnalyticsCounter(String myUrl){
+	AnalyticsCounter(String myUrl) {
 		this.urlFile = myUrl;
 	}
 
 	/**
-	 * This function create the result symptome file
-	 * listResult is a Map with symptomes and their number of recurrence
-	 * 
-	 * The file created is like "Symptome : Number" per line
-	 * 
-	 */
-	public void createFileSymptomeCounter(Map<String, Integer> listResult){
-		//Create result output file
+         * Créer le fichier de sortie, avec les informations des symptomes.
+         * 
+         * @param listResult
+         *            Contient chaque symptome et son nombre d'occurence
+		 * 
+		 * @author SOUICI Yanis
+		 * @version 1.0
+		*/
+	public void createFileSymptomeCounter(Map<String, Integer> listResult) {
+		// Create result output file
 		File file = new File("result.out");
 		BufferedWriter bf = null;
 
+		try {
+			// create new BufferedWriter for the output file
+			bf = new BufferedWriter(new FileWriter(file));
 
-		try {		
-			//create new BufferedWriter for the output file
-			bf = new BufferedWriter( new FileWriter(file) );
+			// iterate map entries
+			for (Map.Entry<String, Integer> entry : listResult.entrySet()) {
 
-			//iterate map entries
-			for(Map.Entry<String, Integer> entry : listResult.entrySet()){
-				
-				//put symptome and value in map 
-				bf.write( entry.getKey() + " : " + entry.getValue() );
-				
+				// put symptome and value in map
+				bf.write(entry.getKey() + " : " + entry.getValue());
+
 				bf.newLine();
 			}
-			
+
 			bf.flush();
-		} catch (Exception e) {			
+		} catch (Exception e) {
 		}
 	}
 
+	/**
+         * Analyse le fichier afin de compter le nombre d'occurence de chaque symptome.
+		 * 
+		 * @return une Map contenant chaque symptome avec son nombre d'occurence
+		 * 
+		 * @throws Exception si le fichier est mal lu
+		 * @author SOUICI Yanis
+		 * @version 1.0
+		 * @since 1.0
+		*/
 	@Override
 	public Map<String, Integer> GetSymptomsRecurrence() throws Exception {
-		// first get input file
-		BufferedReader reader = new BufferedReader (new FileReader(urlFile));
+
+		BufferedReader reader = openFile(urlFile);
 		String line = reader.readLine();
 
 		// Map which contains symptome, recurrence
 		Map<String, Integer> symptomeCounter = new TreeMap<>();
 
-
 		while (line != null) {
 
 			// if first time of symptome, add it in the map
-			if (!symptomeCounter.containsKey(line)){
+			if (!symptomeCounter.containsKey(line)) {
 				symptomeCounter.put(line, 1);
 			} else {
 				// else, increment recurrence of symptome
-				symptomeCounter.put(line, symptomeCounter.get(line)+1);
+				symptomeCounter.put(line, symptomeCounter.get(line) + 1);
 			}
 
-			line = reader.readLine();	// get next symptom
+			line = reader.readLine(); // get next symptom
 		}
 
 		reader.close();
@@ -74,4 +85,19 @@ public class AnalyticsCounter implements ISymptomReader{
 		return symptomeCounter;
 	}
 
+	/**
+         * Ouvre le fichier qui sera traité par le programme.
+         * @see GetSymptomsRecurrence
+		 * 
+		 * @return le ficher contenant la liste des symptomes
+		 * 
+		 * @throws Exception si le fichier est mal lu
+		 * @author SOUICI Yanis
+		 * @version 1.0
+		 * @since 1.0
+		*/
+	public BufferedReader openFile(String fileName) throws FileNotFoundException {
+		BufferedReader reader = new BufferedReader (new FileReader(fileName));
+		return reader;
+	}
 }
